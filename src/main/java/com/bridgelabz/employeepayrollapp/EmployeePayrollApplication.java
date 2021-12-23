1e8a24a5e7313ade6a611cb29f5952fbc5325ada
unknown <khomanesuraj8@gmail.com>
2021-12-23 16:25:42 +0530

    Created Employee payroll Spring boot project and print Welcome message

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Payroll Application");
+		System.out.println("Welcome To Employee Payroll Application");
 		SpringApplication.run(EmployeePayrollApplication.class, args);
 	}
 

ec644628ab3c925a37200006d4f9905f28bb3689
unknown <khomanesuraj8@gmail.com>
2021-12-23 15:45:30 +0530

    Added: Welcome message

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Payroll Application");
+		SpringApplication.run(EmployeePayrollApplication.class, args);
+	}
+
+}

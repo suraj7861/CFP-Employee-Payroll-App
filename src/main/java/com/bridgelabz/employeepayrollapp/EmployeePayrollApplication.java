c1e7b12480f696a3e67bd5d91b26ca3b37efa8a3
unknown <khomanesuraj8@gmail.com>
2021-12-27 15:04:47 +0530

    use lombok library for logging

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -2,13 +2,21 @@ package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
+
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Payroll Application");
-		SpringApplication.run(EmployeePayrollApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment",context.getEnvironment().getProperty("environment"));
+		
+		
 	}
 
 }
